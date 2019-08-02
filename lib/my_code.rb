e0aacb9051c0be96803a1d84a9fd1e372e058ec7
Israel Sanchez <israel.pro1994@gmail.com>
2019-08-02 15:31:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -28,10 +28,7 @@ end
 
 def reduce_to_total(source_array, starting_point=0)
   
-  # if starting_point != nil
-    source_array.reduce(starting_point){|sum, n| sum+ n}
-
-  
+  source_array.reduce(starting_point){|sum, n| sum+ n}
  
 end
 
