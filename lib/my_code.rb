7c013c62166fbc76bb80f36e5e00e6b1d1969328
Israel Sanchez <israel.pro1994@gmail.com>
2019-08-02 15:26:13 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,48 @@
 # My Code here....
+
+def map_to_negativize(source_array)
+  
+source_array.map { |x| x * -1}
+  
+end
+
+def map_to_no_change(source_array)
+  
+source_array.map {|x| x}
+  
+end
+
+def map_to_double(source_array)
+  
+source_array.map { |x| x * 2 }
+  
+end
+
+def map_to_square(source_array)
+  
+source_array.map {|x| x * x}
+  
+end
+
+
+
+def reduce_to_total(source_array, starting_point)
+  
+
+    source_array.reduce(:+)
+  
+ 
+end
+
+  
+def reduce_to_all_true(source_array)
+
+
+
+end 
+
+def reduce_to_any_true(s)
+  
+  
+  
+end
